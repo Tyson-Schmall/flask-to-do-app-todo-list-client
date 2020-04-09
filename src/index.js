85db667255e29a7d62cab210742e1f3ef0335214
Tyson-Schmall <tyson.schmall@gmail.com>
2020-04-09 15:43:34 -0600

    Initial commit, built out some styles (likely temporary) to see that my backend code is functiong properly

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import TodoItem from "./todoItem";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      todo:"",
+      todos: [],
+    };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  renderTodos = () => {
+    return this.state.todos.map(item => {
+      return <TodoItem key={item.id} item={item}/>;
+    });
+  };
+
+  addToDo = e => {
+    e.preventDefault();
+    console.log("ToDo successfully added, thank you.")
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  componentDidMount() {
+    axios
+    .get("https://tas-flask-todo-api.herokuapp.com/todos")
+    .then(res => {
+      this.setState({
+        todos: res.data,
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  render(){
+    return (
+      <div className="app">
+        <h1>ToDo List</h1>
+        <form className="add-todo" onSubmit={this.addTodo}>
+          <input
+            type="text"
+            placeholder="Add Todo"
+            name="todo"
+            onChange={e => this.handleChange(e)}
+            value={this.state.todo}
+            />
+            <button type="submit">Add</button>
+        </form>
+        {this.renderTodos()}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
